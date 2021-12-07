b5345575ddf1340becc2b38d8bc272f4b2e247ee
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-12-07 20:04:17 +0100

    chore: add inline svg support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,16 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-postcss",
     "@storybook/addon-a11y"
-  ]
+  ],
+  babel: async (options) => {
+    options.plugins.push('babel-plugin-inline-react-svg');
+    return options;
+  }
 }
