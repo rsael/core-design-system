87b08b0c5e58c2000409825c5d7a871ba27c5933
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-12-13 18:06:06 +0100

    refactor: update jest and babel config

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+
+// eslint-disable-next-line no-undef
+module.exports = {
+	presets: [
+		"@babel/preset-react",
+		"@babel/preset-env",
+		"@babel/preset-typescript"
+	],
+	plugins: [
+		"macros",
+		"babel-plugin-styled-components"
+	]
+};
