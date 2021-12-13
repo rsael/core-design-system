f33225c4165f1d633c9d21ac099e3866ec0b30ec
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-12-13 23:44:05 +0100

    refactor: add unit tests for Button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,13 +4,43 @@ import "@testing-library/jest-dom";
 import Button from "./Button";
 
 describe("Button", () => {
-	test("should render the button", () => {
-		// render the button and get the getByRole method
+	test("should render primary button", () => {
 		const { getByRole } = render(<Button />);
+		expect(getByRole("button")).toHaveAttribute("kind", "primary");
+	});
 
-		// getByRole as its name gets a selector by its role.
-		// in this case we're looking for a `button`.
-		// then we make sure it's in the document
+	test("should render secondary button", () => {
+		const { getByRole } = render(<Button kind="secondary" />);
+		expect(getByRole("button")).toHaveAttribute("kind", "secondary");
+	});
+
+	test("should render affirmative button", () => {
+		const { getByRole } = render(<Button kind="affirmative" />);
+		expect(getByRole("button")).toHaveAttribute("kind", "affirmative");
+	});
+
+	test("should render negative button", () => {
+		const { getByRole } = render(<Button kind="negative" />);
+		expect(getByRole("button")).toHaveAttribute("kind", "negative");
+	});
+
+	test("should render disabled button", () => {
+		const { getByRole } = render(<Button disabled />);
+		expect(getByRole("button")).toBeDisabled();
+	});
+
+	test("should render disabled button", () => {
+		const { getByRole } = render(<Button disabled />);
+		expect(getByRole("button")).toBeDisabled();
+	});
+
+	test("should render loading button", () => {
+		const { getByRole } = render(<Button loading />);
+		expect(getByRole("button")).toHaveTextContent("loading");
+	});
+
+	test("should render button with icon", () => {
+		const { getByRole } = render(<Button hasIcon />);
 		expect(getByRole("button")).toBeInTheDocument();
 	});
 });
