8f6aafed2863a05d273961608d5bb8d1a24cd892
Ngwa Sedrick Meh <nsedrick101@gmail.com>
2021-12-15 16:01:32 +0100

    update: add toggle for showing and hidding alerts

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -16,23 +16,23 @@ const Template: ComponentStory<typeof Alert> = (args) => <Alert {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     kind: "primary",
-    children: "Neutral info displayed here. Carry on."
+    message: "Neutral info displayed here. Carry on."
 };
 
 export const Affirmative = Template.bind({});
 Affirmative.args = {
     kind: "affirmative",
-    children: "A successful message appears to those who did well"
+    message: "A successful message appears to those who did well"
 };
 
 export const Negative = Template.bind({});
 Negative.args = {
     kind: "negative",
-    children: "An error message displayed when something went wrong"
+    message: "An error message displayed when something went wrong"
 };
 
 export const Warning = Template.bind({});
 Warning.args = {
     kind: "warning",
-    children: "Warning text here so you can be more careful"
+    message: "Warning text here so you can be more careful"
 };
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,4 +1,4 @@
-import React, { AriaRole, ReactNode } from "react";
+import React, { AriaRole, ReactNode, MouseEvent, useState } from "react";
 import tw from "twin.macro";
 import styled from "styled-components";
 import {
@@ -14,14 +14,17 @@ export interface AlertProps {
     children?: ReactNode;
     message?: string;
     onClose?: () => void;
+    role?: AriaRole;
+    visible?: boolean;
 }
 
-const StyledAlert = styled.div(({ kind }: AlertProps) => [
-    tw`flex items-center justify-between px-6 py-4 text-sm text-gray-500 border-l-4 appearance-none`,
+const StyledAlert = styled.div(({ kind, visible }: AlertProps) => [
+    tw`flex items-center justify-between px-6 py-4 text-base font-medium border-l-4 appearance-none`,
     kind === "primary" && tw`border-primary bg-primary-subtle`,
     kind === "affirmative" && tw`bg-green-100 border-green-300 `,
     kind === "negative" && tw`bg-red-100 border-red-300 `,
-    kind === "warning" && tw`bg-yellow-100 border-yellow-400 `
+    kind === "warning" && tw`bg-yellow-100 border-yellow-400 `,
+    !visible && tw`hidden`
 ]);
 
 const Container = styled.div(() => [tw`flex place-items-center`]);
@@ -31,66 +34,77 @@ const Alert = ({
     kind,
     message,
     onClose,
+    role,
+    visible,
     ...props
 }: AlertProps): JSX.Element => {
+    // alert close handler
+    function handleClose() {
+        onClose ? onClose() : null;
+    }
+
     switch (kind) {
         case "affirmative": {
             return (
-                <StyledAlert kind={kind} {...props} role="alert">
+                <StyledAlert
+                    kind={kind}
+                    role={role}
+                    visible={visible}
+                    {...props}
+                >
                     <Container>
-                        <FiCheckCircle
-                            size={18}
-                            tw="text-green-300 mr-8"
-                            onClick={onClose}
-                        />
-                        {children || message || "Alert"}
+                        <FiCheckCircle size={18} tw="text-green-300 mr-8" />
+                        {message || children || "Alert"}
                     </Container>
-                    <FiX />
+                    <FiX onClick={() => handleClose()} />
                 </StyledAlert>
             );
         }
         case "negative": {
             return (
-                <StyledAlert kind={kind} {...props} role="alert">
+                <StyledAlert
+                    kind={kind}
+                    role={role}
+                    visible={visible}
+                    {...props}
+                >
                     <Container>
-                        <FiXCircle
-                            size={18}
-                            tw="text-red-300 mr-8"
-                            onClick={onClose}
-                        />
-                        {children || message || "Alert"}
+                        <FiXCircle size={18} tw="text-red-300 mr-8" />
+                        {message || children || "Alert"}
                     </Container>
-                    <FiX />
+                    <FiX onClick={() => handleClose()} />
                 </StyledAlert>
             );
         }
         case "warning": {
             return (
-                <StyledAlert kind={kind} {...props} role="alert">
+                <StyledAlert
+                    kind={kind}
+                    role={role}
+                    visible={visible}
+                    {...props}
+                >
                     <Container>
-                        <FiAlertTriangle
-                            size={18}
-                            tw="text-yellow-400 mr-8"
-                            onClick={onClose}
-                        />
-                        {children || message || "Alert"}
+                        <FiAlertTriangle size={18} tw="text-yellow-400 mr-8" />
+                        {message || children || "Alert"}
                     </Container>
-                    <FiX />
+                    <FiX onClick={() => handleClose()} />
                 </StyledAlert>
             );
         }
         default: {
             return (
-                <StyledAlert kind={kind} {...props} role="alert">
+                <StyledAlert
+                    kind={kind}
+                    role={role}
+                    visible={visible}
+                    {...props}
+                >
                     <Container>
-                        <FiAlertCircle
-                            size={18}
-                            tw="text-primary mr-8"
-                            onClick={onClose}
-                        />
-                        {children || message || "Alert"}
+                        <FiAlertCircle size={18} tw="text-primary mr-8" />
+                        {message || children || "Alert"}
                     </Container>
-                    <FiX />
+                    <FiX onClick={() => handleClose()} />
                 </StyledAlert>
             );
         }
@@ -98,7 +112,9 @@ const Alert = ({
 };
 
 Alert.defaultProps = {
-    kind: "primary"
+    kind: "primary",
+    role: "alert",
+    visible: true
 };
 
 export default Alert;
